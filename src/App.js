dbe515acc2ba21e80ee2954746412ef185b65351
KyleVo2024 <112658953+KyleVo2024@users.noreply.github.com>
2024-01-16 14:24:05 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import './App.css';
 import React, { useState } from 'react';
 function App() {
   const [cookies, setCookies] = useState(0);
+  const [grandma, setgrandma] = useState(0);
   var cookie = 1/cookies+"s";
+  const Store = [
+    {
+      "Name": "Grandma",
+      "Cost": 100,
+      "CPS": 10
+    },
+    {
+      "Name": "farms",
+      "Cost": 100,
+      "CPS": 10
+    }
+  ];
   function clicked() {
     setCookies(cookies + 1)
-    cookie = 1/(Math.log(cookies)+1)+"s";
+    cookie = 1 / (Math.log(cookies) + 1) + "s";
     document.documentElement.style
-      .setProperty('--cookies',cookie);
+      .setProperty('--cookies', cookie);
+  }
+
+  function buy(Name) {
+    setgrandma(grandma +1)
   }
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Christmas Clicker
-        </p>
-        {cookies}
+      {cookies}
+
         <img src={logo} className="App-logo" alt="logo" onClick={() => clicked()}></img>
+        <button onClick={this.buy(Name)}>Duck</button>
+        {grandma}
       </header>
     </div>
   );
